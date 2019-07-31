3494d90ee0cadda1ec507b70ed891f83809aecfe
Stephen Gilbert <sgilbert@student.cccd.edu>
2019-07-31 19:09:37 +0000

    Add homework 1

---
+++ h01/h01.cpp
@@ -0,0 +1,20 @@
+/**
+ *  @file h01.cpp
+ */
+#include <iostream>
+using namespace std;
+
+string STUDENT = "WHO ARE YOU?";  // Add your Canvas login name
+extern string ASSIGNMENT;
+
+/**
+ * One line describing what this program does.
+ * @return 0 for success.
+ */
+int run()
+{
+    // Write your code here
+
+    return 0;
+}
+
